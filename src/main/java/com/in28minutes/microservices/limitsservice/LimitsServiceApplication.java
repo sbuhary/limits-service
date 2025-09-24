379e27c37340fce60ff1313a091d007c6e016aee
Shihaaz Buhary <shihaazbz@gmail.com>
2025-09-24 12:55:47 +0530

    refactoring for in28minutes

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sbuhary.microservices.limits_service;
+package com.in28minutes.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sbuhary.microservices.limits_service;
+package com.in28minutes.microservices.limitsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
